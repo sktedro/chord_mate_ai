b3ba48c7938a870e4d0f083afbe16386370eadc8
tedro <sktedro@gmail.com>
2021-12-26 19:27:23 +0100

    Recognizing notes needed for a chord working
    (not properly though, I suppose)

--- gen_chords.py
+++ gen_chords.py
@@ -8,8 +8,8 @@ import subprocess
 ## SETTINGS ##
 ##############
 
-notesPath = "../audio/notes"
-chordsPath = "../audio/chords"
+notesPath = "./audio/notes"
+chordsPath = "./audio/chords"
 
 # If set to true, orders of notes will be also picked randomly
 # (the generated chord could consist of A0, B7, for example)
@@ -75,8 +75,78 @@ def pickChord(chords, targets):
         target = ""
     elif target == "minor":
         target = "m"
-    return chord + target
+    return chord, target
 
+
+def ls(path):
+    return subprocess.run(["ls", path], stdout=subprocess.PIPE).stdout.decode('utf-8').split("\n")[0: -1]
+
+def getPitchClassesOfNotesInChord(chordType):
+    # Major (C)
+    if chordType == "":
+        return [0, 4, 7]
+
+    # Minor (Cm)
+    elif chordType == "m":
+        return [0, 3, 7]
+
+    # Dominant seventh (C7)
+    elif chordType == "7":
+        return [0, 4, 7, 10]
+
+    # Power chord (fifth chord) (C5)
+    elif chordType == "5":
+        return [0, 7]
+
+    # Diminished chord (Cdim)
+    elif chordType == "dim":
+        return [0, 3, 6]
+
+    # Diminished seventh chord (Cdim7)
+    elif chordType == "dim7":
+        return [0, 3, 6, 9]
+
+    # Augmented chord (Caug, C+)
+    elif chordType == "aug":
+        return [0, 4, 8]
+
+    # Suspended second chord (Csus2)
+    elif chordType == "sus2":
+        return [0, 2, 7]
+
+    # Suspended fourth chord (Csus4)
+    elif chordType == "sus4":
+        return [0, 5, 7]
+
+    # Major seventh (Cmaj7)
+    elif chordType == "maj7":
+        return [0, 4, 7, 11]
+
+
+
+def getNotesInChord(chord, chordType, order):
+    notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"] * 2
+
+    rootIndex = notes.index(chord)
+
+    pitchClasses = getPitchClassesOfNotesInChord(chordType)
+
+    outputNotes = []
+
+    for pc in pitchClasses:
+        # Get the note index in notes arr
+        index = rootIndex + pc
+
+        # Get the note order
+        noteOrder = order
+        if index >= 12:
+            noteOrder += 1
+
+        # Now we can compose the note
+        outputNotes.append(notes[index] + str(noteOrder))
+
+    print(outputNotes)
+    return outputNotes
 
 
 ##########
@@ -88,23 +158,41 @@ def main():
     amount, targets = handleArguments()
     chords = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 
-    # Get all paths to all the .wav note files
-    instrumentsCommand = ("ls " + notesPath).split(" ")
-    instruments = subprocess.run(instrumentsCommand, stdout=subprocess.PIPE).stdout.decode('utf-8').split("\n")[0: -1]
+    # Get all instruments (folders in notesPath)
+    instruments = ls(notesPath)
+
+    # Get all notes for each instrument (.wav files in notesPath/instrument)
+    notesFiles = []
+    for instrument in instruments:
+        notesFiles.append(ls(notesPath + "/" + instrument))
+    # Filter out ones that don't end with .wav
+    #  for i in range(len(notesFiles)):
+        #  notesFiles[i] = [note for note in notesFiles[i] if ".wav" in note]
+
+
+
 
     # For amount
     for i in range(amount):
 
         # Randomly pick a chord (from len(targets) * len(chords) combinations)
         #  chord = chords[np.random.randint(len(chords))] + targets[np.random.randint(len(targets))]
-        chord = pickChord(chords, targets)
+        plainChord, chordType = pickChord(chords, targets)
+        chord = plainChord + chordType
         print("Generating", chord)
 
         # Randomly pick an instrument
         instrument = instruments[np.random.randint(len(instruments))]
         print("Instrument picked: ", instrument)
 
+        # TODO Randomly pick an order
+        order = 2
+
+        # Get a list of notes contained in the picked chord
+        useNotes = getNotesInChord(plainChord, chordType, order)
+
         # Filter the note files to ones contained in the picked chord
+        useNotesFiles = notesFiles[instruments.index(instrument)]
 
         # Sort the note files into arrays based on their notes
 
