913111522675b8f62a1fd8d536b034b0d3151fac
tedro <sktedro@gmail.com>
2021-12-30 01:35:39 +0100

    Baseline for generating chords from nsynth done

--- gen_from_nsynth.py
+++ gen_from_nsynth.py
@@ -10,15 +10,15 @@ dsPath = "/media/tedro/#2/a_only_on_external/d_projekty/ai/chord_mate_ai/dataset
 
 def filterDs(x, instrument, label, source, pitch):
     #  return tf.equal(x["instrument"]["family"], instrument) and tf.equal(x["instrument"]["label"], label)
-    return ((tf.equal(x["instrument"]["family"], instrument) or instrument == -1)
-            and (tf.equal(x["instrument"]["label"], label) or label == -1)
-            and (tf.equal(x["instrument"]["source"], source) or label == -1)
-            and (tf.equal(x["pitch"], pitch) or label == -1)
+    return ((instrument == -1 or tf.equal(x["instrument"]["family"], instrument))
+            and (label == -1 or  tf.equal(x["instrument"]["label"], label))
+            and (source == -1 or tf.equal(x["instrument"]["source"], source))
+            and (pitch == -1 or  tf.equal(x["pitch"], pitch))
             )
 
 def main():
     # TODO Choose a target
-    # Amounts:
+    # Amounts in targets:
     # train: 60,788
     # test:  8,518
     # valid: 17,469
@@ -47,21 +47,22 @@ def main():
     family = 3
 
     # Choose instrument label (1006 classes)
+    # We probably won't, ever - label seems to be unique in the whole dataset
+    # for an instrument family (eg. keyboard_electronic_054 has label 480)
     label = -1
 
     # Choose instrument source (3 classes)
     # 0 acoustic
     # 1 electronic
     # 2 synthetic
-    source = 0
+    source = -1
 
-    # Choose pitch (128 classes)
-    pitch = 56
+    # Choose pitch (128 classes) - 0 = C-1, 12 = C0, C1 = 24, ...
+    pitch = 36
 
     filteredDs = ds.filter(lambda x: filterDs(x, family, label, source, pitch))
 
     filteredData = filteredDs.take(1)
-    print(filteredData)
     for note in filteredData:
         print(note)
         rawSignal = note["audio"].numpy()
@@ -73,6 +74,8 @@ def main():
             channels=1
         )
         play(audio)
+        break
+    print("Done")
 
 
 if __name__ == "__main__":
